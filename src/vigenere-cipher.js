0da5cec2ef5d8d60a996c3e50a4a9e95a2d275e0
madinarsp <madinarsp20@gmail.com>
2022-05-03 00:54:51 +0600

    refactor: add solution of vigenere-cipher from previous season

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(boolVal) {
+    if(boolVal == undefined || boolVal) this.modification = "direct";
+    else this.modification = "reverse";
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if(!message || !key) throw new Error('Incorrect arguments!');
+    let keyIndex = 0;
+    let encryptedStr = "";
+    let messageStr = message.toUpperCase();
+    let keyStr = key.toUpperCase();
+    for(let i=0; i<messageStr.length; i++) {
+      if(messageStr.charCodeAt(i) >= 65 && messageStr.charCodeAt(i) <= 90) {    // 60 & 90 are ascii codes for 'A' & 'Z', respectively
+        let encrCharCode = 65 + (messageStr.charCodeAt(i) + keyStr.charCodeAt(keyIndex) - 65*2) % 26;  //26 letters
+        encryptedStr += String.fromCharCode(encrCharCode);
+        if(keyIndex == keyStr.length - 1) keyIndex = 0;
+        else keyIndex++;
+      }
+      else encryptedStr += messageStr[i];
+    }
+    if(this.modification == "reverse") return encryptedStr.split("").reverse().join("");
+    else return encryptedStr;
+  }
+  decrypt(encryptedMessage, key) {
+    if(!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+    let keyIndex = 0;
+    let decryptedStr = "";
+    let encrMessageStr = encryptedMessage.toUpperCase();
+    let keyStr = key.toUpperCase();
+    for(let i=0; i<encrMessageStr.length; i++) {
+      if(encrMessageStr.charCodeAt(i) >= 65 && encrMessageStr.charCodeAt(i) <= 90) {
+        let decrCharCode = 65 + (encrMessageStr.charCodeAt(i) - keyStr.charCodeAt(keyIndex) + 26) % 26;
+        decryptedStr += String.fromCharCode(decrCharCode);
+        if(keyIndex == keyStr.length - 1) keyIndex = 0;
+        else keyIndex++;
+      }
+      else decryptedStr += encrMessageStr[i];
+    }
+    if(this.modification == "reverse") return decryptedStr.split("").reverse().join("");
+    else return decryptedStr;
   }
 }
 
