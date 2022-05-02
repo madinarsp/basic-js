c0f6c2106e292591b67c0a483fcc28e67d37a4af
madinarsp <madinarsp20@gmail.com>
2022-05-03 00:23:48 +0600

    refactor: add solution to dream-team from previous season

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if(typeof members != "object" || members == null) return false;
+  let secretName = [];
+  for(let i=0; i<members.length; i++) {
+    if(typeof(members[i]) == "string") {
+      secretName.push(members[i].trim()[0].toUpperCase());
+    }
+  }
+  return secretName.sort().join("");
 }
 
 module.exports = {
