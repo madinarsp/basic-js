9aa5f9d389454fbb2ef8c291782c26edd8eb3cfa
madinarsp <madinarsp20@gmail.com>
2022-05-03 00:20:37 +0600

    refactor: add solution to carbon-dating from previous season

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let sampleActNum = Number.parseFloat(sampleActivity);
+  if( typeof(sampleActivity)!='string' || Number.isNaN(sampleActNum) || sampleActNum <= 0 ) return false;
+  let age = Math.ceil( Math.log(MODERN_ACTIVITY/sampleActNum) * HALF_LIFE_PERIOD / 0.693 );
+  return (age > 0) ? age : false;
 }
 
 module.exports = {
